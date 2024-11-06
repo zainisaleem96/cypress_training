ae8a8fd0e9980996c30a1acad705817906a21882
ZainabIdrees <zainabidrees@192.168.10.23>
2024-11-07 00:46:22 +0500

    First commit

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,34 @@
+
+import HomePage from '../support/home';
+
+context('Actions', ()=> {
+
+    const homePage = new HomePage();
+
+    beforeEach(()=>{
+        cy.visit('https://demoqa.com/',{ failOnStatusCode: false});
+        cy.get
+    })
+
+  /*  it('Verify reached successfully', ()=> {
+        homePage.verifyPageLoadedSuccessfully();
+ })
+    it('Verify total number of menus', ()=> {
+        homePage.verifyTotalNumberofItems();
+})
+    it('Verify each menu item', ()=> {
+    homePage.verifyEachMenu();
+})  
+it('Verify Elements menu item', ()=> {
+  homePage.verifyElements();
+
+})  
+it('Verify CheckBox menu item', ()=> {
+  homePage.verifyCheckBox();
+
+})*/
+it('Verify CheckBox menu item', ()=> {
+  homePage.verifyRadioButton();
+
+})
+})
---
+++ cypress/support/home.ts
@@ -0,0 +1,51 @@
+import { should } from "chai";
+
+const data = require('../fixtures/users')
+
+class HomePage{
+
+    verifyPageLoadedSuccessfully(){
+         cy.title().should('eq', 'DEMOQA');
+         cy.url().should('include', 'https://demoqa.com/');
+         cy.viewport('macbook-11');
+     }
+ 
+     verifyTotalNumberofItems(){
+        // cy.visit('seleniumPractise/#/',{ failOnStatusCode: false});
+         cy.get('.card-body:visible').should('have.length',6)
+     }
+
+     verifyEachMenu(){
+        cy.get('.category-cards').find('.card-body').each(($el, index, $list) => {
+
+            var MenuText = $el.find('h5').text()
+            console.log(MenuText);
+            })
+
+    }
+    verifyElements(){
+        cy.get('.card-body h5').contains('Elements').click();
+        cy.get(':nth-child(1) > .element-list > .menu-list > #item-0').click();
+        cy.get('#userName').type(data.fullname);
+        cy.get('#userEmail').type(data.email);
+        cy.get('#currentAddress').type(data.current_address);
+        cy.get('#permanentAddress').type(data.permanent_address);
+        cy.get('#submit').click();
+    }
+    verifyCheckBox(){
+        cy.get('.card-body h5').contains('Elements').click();
+        cy.get('#item-1').click();
+        //cy.get('.rct-title').should('have.text','Home').click();
+        cy.get('.rct-collapse').click();
+        cy.get('.rct-title')
+  .should('have.text', 'Documents').click();
+}
+    verifyRadioButton(){
+        cy.get('.card-body h5').contains('Elements').click();
+        cy.get('#item-2').click();
+        cy.get(':nth-child(3) > :nth-child(3)').click();
+    }
+
+    
+}
+export default HomePage
